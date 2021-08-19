df98b1b28e3b2bf592b96f485bab2ca26cdcfb86
aavojtek <aavojtek@mts.ru>
2021-08-19 14:55:00 +0300

    0905. Sort Array by Parity

---
+++ tests/v0900/test_0905_sort_array_by_parity.py
@@ -0,0 +1,16 @@
+""" 0905. Sort Array by Parity """
+
+import pytest
+from leetcode.v0900._0905_sort_array_by_parity import Solution
+
+
+@pytest.mark.t0905
+@pytest.mark.parametrize(
+    'value, expected',
+    [
+        ([3,1,2,4], [2,4,3,1]),
+        ([0], [0]),
+    ]
+)
+def test_two_sums(value, expected):
+    assert Solution().sortArrayByParity(value) == expected
