3503661c5a18f3439593d43ec96e0a7e759a14fb
vtb4059174 <vtb4059174@vtb.ru>
2022-09-05 09:20:23 +0300

    Добавлена ссылка на задачу

--- leetcode/problems/V0300/T0326_power_of_three.py
+++ leetcode/problems/V0300/T0326_power_of_three.py
@@ -1,5 +1,7 @@
 """ 0326. Power of Three
 
+Link: https://leetcode.com/problems/power-of-three
+
 Given an integer n, return true if it is a power of three. Otherwise, return false.
 
 An integer n is a power of three, if there exists an integer x such that n == 3x.
