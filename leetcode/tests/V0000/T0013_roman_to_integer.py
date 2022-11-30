9e8573eae425642beff5de52ec15b6fd7e9aba69
vtb4059174 <vtb4059174@vtb.ru>
2022-11-30 09:24:18 +0300

    0013. Roman to Integer

---
+++ leetcode/tests/V0000/T0013_roman_to_integer.py
@@ -0,0 +1,18 @@
+""" 0013. Roman to Integer """
+
+import pytest
+from leetcode.problems.V0000.T0013_roman_to_integer import Solution
+
+
+@pytest.mark.T0013
+@pytest.mark.parametrize(
+    'value, expected',
+    [
+        ('III', 3),
+        ('IV', 4),
+        ('LVIII', 58),
+        ('MCMXCIV', 1994),
+    ]
+)
+def test_integer_to_roman(value, expected):
+    assert Solution().romanToInt(value) == expected
