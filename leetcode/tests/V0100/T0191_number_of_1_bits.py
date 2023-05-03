97ceec0018737a65b89cbf81bfcb53b012b8a6fe
vtb4059174 <vtb4059174@vtb.ru>
2023-05-03 12:13:53 +0300

    0191. Number of 1 Bits

---
+++ leetcode/tests/V0100/T0191_number_of_1_bits.py
@@ -0,0 +1,17 @@
+""" 0191. Number of 1 Bits """
+
+import pytest
+from leetcode.problems.V0100.T0191_number_of_1_bits import Solution
+
+
+@pytest.mark.T0191
+@pytest.mark.parametrize(
+    'n, result',
+    [
+        (11, 3),
+        (128, 1),
+        (4294967293, 31),
+    ]
+)
+def test_two_sum(n, result):
+    assert Solution().hammingWeight(n) == result
