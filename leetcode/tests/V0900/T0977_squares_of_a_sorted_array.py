0da91f1bf240c0aacdfc47efc01749c9873cb302
vtb4059174 <vtb4059174@vtb.ru>
2023-05-12 10:49:55 +0300

    T0907: Squares of a Sorted Array

---
+++ leetcode/tests/V0900/T0977_squares_of_a_sorted_array.py
@@ -0,0 +1,16 @@
+""" 0977. Squares of a Sorted Array """
+
+import pytest
+from leetcode.problems.V0900.T0977_squares_of_a_sorted_array import Solution
+
+
+@pytest.mark.T0977
+@pytest.mark.parametrize(
+    'value, expected',
+    [
+        ([-4, -1, 0, 3, 10], [0, 1, 9, 16, 100]),
+        ([-7, -3, 2, 3, 11], [4, 9, 9, 49, 121]),
+    ]
+)
+def test_sort_array_by_parity(value, expected):
+    assert Solution().sortedSquares(value) == expected
